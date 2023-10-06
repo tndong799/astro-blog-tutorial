35cf3a659923fb89120c4f35c480300b596faad1
dongtn1 <dongtn1@fireapps.vn>
2023-10-06 13:06:42 +0700

    add-darkmode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
-    site: 'https://master--stirring-basbousa-2b1aa9.netlify.app/',
+  integrations: [tailwind(), preact()],
+  site: 'https://master--stirring-basbousa-2b1aa9.netlify.app/'
 });
